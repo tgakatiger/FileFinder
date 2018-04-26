f77e38e0ef4091862385e1410dca5ad3160b6bc8
Tg Tiger <tgakatiger@gmail.com>
2018-04-26 22:33:50 +0300

    Test passwords with Console class

--- src/core/ConsoleClass.java
+++ src/core/ConsoleClass.java
@@ -1,15 +1,25 @@
 package core;
 
 import java.io.Console;
+import java.util.Scanner;
 
 public class ConsoleClass {
     public ConsoleClass() {
         Console console = System.console();
+        if (console == null) {
+            System.out.println("No console found!");
+            System.exit(-1);
+        }
         System.out.println("Your name:");
         String name = console.readLine();
-//        System.out.println("Password:");
-//        String password = console.readPassword().toString();
-        System.out.println(String.format("Hello, %s. You pass => $s", name));
+        System.out.println("Password:");
+        char[] password = console.readPassword();
+        System.out.println(String.format("Hello, %s", name));
+        System.out.println("You password is");
+        for (char ch : password) {
+            System.out.print(ch);
+        }
+
     }
 
 }
